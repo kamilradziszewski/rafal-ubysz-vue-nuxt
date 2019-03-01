981413adcb967e7f3e764d40702f65fa73dd2df5
Kamil Radziszewski <contact@kamilradziszewski.com>
2019-03-01 15:24:24 +0100

    Add fake Addresses object

---
+++ assets/addresses.js
@@ -0,0 +1,44 @@
+export const addresses = [
+  {
+    map: {
+      lat: '',
+      lng: ''
+    },
+    title: 'Wojewódzki Szpital Zakaźny',
+    additional_info: 'Zakład Radiologii',
+    street: 'ul. Wolska 37',
+    telephone_1: '22 33 55 247',
+    telephone_2: '',
+    webpage: 'http://www.zakazny.pl',
+    opening_hours_1: 'PN-PT 8:00-14:00',
+    opening_hours_2: ''
+  },
+  {
+    map: {
+      lat: '',
+      lng: ''
+    },
+    title: 'TS klinika',
+    additional_info: '',
+    street: 'ul. Górczewska 228E',
+    telephone_1: '22 304 18 55',
+    telephone_2: '0 605 088 288',
+    webpage: 'http://www.tsklinika.pl',
+    opening_hours_1: 'PN 15:45-18:00',
+    opening_hours_2: 'CZW 15:00-18:00'
+  },
+  {
+    map: {
+      lat: '',
+      lng: ''
+    },
+    title: 'Centrum Medyczne MIŁA',
+    additional_info: '',
+    street: 'Al. Miła 6',
+    telephone_1: '22 887 88 44',
+    telephone_2: '',
+    webpage: 'https://www.cmmila6.pl',
+    opening_hours_1: 'SB 11:00-15:00',
+    opening_hours_2: ''
+  }
+]
