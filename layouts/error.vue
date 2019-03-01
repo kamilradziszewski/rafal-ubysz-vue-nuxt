32896e3e50c4995fa57602bcc52071699d762179
Kamil Radziszewski <contact@kamilradziszewski.com>
2019-03-01 15:46:29 +0100

    Add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <article
+    class="align-center grid-x grid-padding-x"
+    :class="this.$route.name"
+  >
+    <section class="article-content small-11 medium-8 cell">
+      <h1 class="article-h1">
+        Error Page
+      </h1>
+      <div
+        class="article-body"
+      >
+        Strona o podanym adresie nie istnieje.
+      </div>
+    </section>
+  </article>
+</template>
