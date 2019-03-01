82e5858e369112d6f00668c96009cafd552046ff
Kamil Radziszewski <contact@kamilradziszewski.com>
2019-03-01 15:36:14 +0100

    Add AddressCard component

---
+++ components/AddressCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="cell small-12 medium-4 large-3">
+    <div class="address-card">
+      <h3 class="address-title">
+        {{ address.title }}
+      </h3>
+      <p>{{ address.additional_info }}</p>
+      <p>{{ address.street }}</p>
+      <p />
+      <p>
+        <a class="address-telephone" href="tel:+48223355247">
+          Tel. 22 33 55 247
+        </a>
+      </p>
+      <a class="address-webpage" href="http://www.zakazny.pl" target="_blank">
+        www.zakazny.pl
+      </a>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    address: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer id="footer">
+    <div class="grid-x">
+      <div class="cell large-3 icon-map-column">
+        <svg class="icon-map">
+          <use xlink:href="~/assets/image/icons-sprite.svg#icon-map" />
+        </svg>
+      </div>
+
+      <AddressCard v-for="address in loadedAddresses" :key="address.id" :address="address" />
+    </div>
+    <div class="dividing-line" />
+
+    <p class="copyright">
+      Copyright {{ currentYear }} Rafał Ubysz
+    </p>
+  </footer>
+</template>
+
+<script>
+import AddressCard from '~/components/AddressCard'
+
+export default {
+  components: {
+    AddressCard
+  },
+  data() {
+    return {
+      currentYear: (() => {
+        const currentDate = new Date()
+        return currentDate.getFullYear()
+      })()
+    }
+  },
+  computed: {
+    loadedAddresses() {
+      return this.$store.getters.loadedAddresses
+    }
+  }
+}
+</script>
